06543dbb0ff4a370f0413fb68f35664377af4142
Yu Li <yli0607x@gmail.com>
2018-10-05 16:51:22 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,13 @@ def first_challenge
   }
 
   #your code here
-contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
-
+#contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
+contacts["Freddy Mercury"].each do |attribute, flavor|
+  value.delete_if do |flavor|
+    flavor == "strawberry"
+end
+end
+end
   #remember to return your newly altered contacts hash!
   contacts
 end
