f4d14fe939248394e8b3dc4f27387d5c9e15faf4
Yu Li <yli0607x@gmail.com>
2018-10-05 16:45:30 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-contact["Freddy Mercury"][:favorite_icecream_flavors].shift
+contacts["Freddy Mercury"][:favorite_icecream_flavors].shift
 
   #remember to return your newly altered contacts hash!
   contacts
